102043b6fdf13ab03dcf64116ba63340ea48fab3
Guru081 <gurubasavaraj1996@gmail.com>
2020-01-07 19:09:34 +0530

    new

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import { hydrate, render } from "react-dom";
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
+  console.log('hydrate')
 } else{
   render(<App />, rootElement);
+  console.log('render')
+
 }
 // ReactDOM.render(<App />, document.getElementById("root"))
 // ReactDOM.render(<App />, rootElement);
